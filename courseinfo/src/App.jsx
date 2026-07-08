f03cdbea0e0a9e978cfb055d70f910ada0c2af39
zakwatsonn <zgwatson@outlook.com>
2026-07-08 11:41:50 +0100

    exercise2.4

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -28,36 +28,60 @@ const Course = ({ course, total }) => {
 }
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-      {
-        name: 'Mystery Part',
-        exercises: 6,
-        id: 4,
-      }
-    ],
-  }
+  const courses = [
+    {
+      id: 1,
+      name: 'Half Stack application development',
+      parts: [
+        {
+          name: 'Fundamentals of React',
+          exercises: 10,
+          id: 1
+        },
+        {
+          name: 'Using props to pass data',
+          exercises: 7,
+          id: 2
+        },
+        {
+          name: 'State of a component',
+          exercises: 14,
+          id: 3
+        },
+        {
+          name: 'Mystery Part',
+          exercises: 6,
+          id: 4,
+        }
+      ],
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
 
-  const total = course.parts.reduce((accumulator, currentValue) => accumulator + currentValue.exercises, 0)
+  const total = courses.map(course => 
+    course.parts.reduce((sum, current) => 
+      sum + current.exercises,
+      0
+    )
+  )
+  console.log(total)
 
-  return <Course course={course} total={total} />
+  return (courses.map((course) => <Course course={course} total={total[course.id-1]}/>))
 }
 
 export default App
