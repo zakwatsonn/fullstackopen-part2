c83b054bed811db5bf5e8a1620cab2618a1d8966
zakwatsonn <zgwatson@outlook.com>
2026-07-09 16:37:58 +0100

    ex2.11 feat db.json fetch using axios and effect

--- phonebook/src/App.jsx
+++ phonebook/src/App.jsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Filter from './components/Filter'
 import Form from './components/Form'
 import Numbers from './components/Numbers'
+import axios from 'axios'
 
 const Profile = (props) => {
   return (
@@ -12,16 +13,22 @@ const Profile = (props) => {
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameFilter, setNameFilter] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('server responds with', response)
+        setPersons(response.data)
+      })
+  }, [])
+
   const personsToShow = persons.filter(person => 
     person.name.toLowerCase().includes(nameFilter.toLowerCase())
   )
